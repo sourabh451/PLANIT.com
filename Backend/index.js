fdc5aac7727626489148a6961e96dd17c22c46a8
sourabh451 <er.sourabhapatil@gmail.com>
2023-08-29 16:33:04 +0530

    BE task completed

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,11 @@
 let express = require("express");
 let app = express();
 let cors = require("cors");
+let chartjs = require("chart.js");
 require("dotenv").config();
 
 const { connection } = require("./db");
+const { taskRouter } = require("./routes/tasks.routes");
 const { userRouter } = require("./routes/users.routes");
 const { auth } = require("./middlewares/auth.middleware");
 
@@ -12,6 +14,7 @@ app.use(cors());
 
 app.use("/users", userRouter);
 app.use(auth);
+app.use("/tasks", taskRouter);
 
 app.listen(process.env.port, async () => {
   try {
